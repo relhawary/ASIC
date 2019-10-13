efb34d67b6618953eaa7a0e5b99bd53d91dc3aba
relhawary <roeyaelhawary@aucegypt.edu>
2019-10-13 23:49:53 +0200

    oki

--- HW2/prbs_verify.vhd
+++ HW2/prbs_verify.vhd
@@ -23,7 +23,7 @@ ARCHITECTURE prbs_verify_arch OF prbs_verify IS
 	CONSTANT in_data_rom : std_logic_vector(95 DOWNTO 0) := x"ACBCD2114DAE1577C6DBF4C9";
 	CONSTANT out_data_rom : std_logic_vector(95 DOWNTO 0) := x"558AC4A53A1724E163AC2BF9";
 
-	SIGNAL counter : unsigned(6 DOWNTO 0);-- := "0000000";
+	SIGNAL counter : std_logic_vector(6 DOWNTO 0);-- := "0000000";
 	SIGNAL data_in_bit, data_out_bit : std_logic;
 	SIGNAL data_out_vector : std_logic_vector(95 DOWNTO 0);
 	--signal pass_out: std_logic:='0';
@@ -44,21 +44,21 @@ BEGIN
 		IF (reset = '1') THEN
 			counter <= "1011111";
 			pass_out <= '0';
-			ELSIF (clk'event AND clk = '1') THEN
-			IF ((to_integer(counter) < 96) AND en = '1') THEN
+		ELSIF (clk'event AND clk = '1') THEN
+			IF ((to_integer(unsigned(counter)) < 96) AND en = '1') THEN
 				--data_in_bit <= in_data_rom(to_integer(unsigned((counter))));
-				data_out_vector(to_integer(counter)) <= data_out_bit;
-				counter <= (counter - "0001");
+				data_out_vector(to_integer(unsigned(counter))) <= data_out_bit;
+				counter <= (std_logic_vector(unsigned(counter) - "0001"));
 				--counter<= std_logic_vector(unsigned(counter)+ 1);
-				ELSIF ((to_integer(counter)) >= 95 AND en = '1') THEN
+			ELSIF (to_integer(unsigned(counter)) >= 95 AND en = '1') THEN
 				IF (data_out_vector = out_data_rom) THEN
 					pass_out <= '1';
-					ELSIF (data_out_vector /= out_data_rom) THEN
+				ELSIF (data_out_vector /= out_data_rom) THEN
 					pass_out <= '0';
 				END IF;
 			END IF;
 		END IF;
 	END PROCESS;
-	data_in_bit <= in_data_rom(to_integer(counter)) WHEN((to_integer(counter) >= 0) AND (to_integer(counter)) < 96)ELSE
-	'0';
+	data_in_bit <= in_data_rom(to_integer(unsigned((counter)))) WHEN(to_integer(unsigned(counter)) >= 0) AND (to_integer(unsigned(counter)) < 96)ELSE
+		'0';
 END prbs_verify_arch;
